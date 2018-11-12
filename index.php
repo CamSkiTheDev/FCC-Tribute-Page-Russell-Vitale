d3aaa5a4294a216c41c1365d2f33394f5b1b2fb6
CamSkiTheDev <44956222+CamSkiTheDev@users.noreply.github.com>
2018-11-11 19:52:11 -0700

    Update

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Anton|Lobster|Open+Sans|Pacifico|Roboto">
+  <link rel="stylesheet" href="./assets/css/style.css">
   <title>Russell Vitale - Tribute Page by Cameron Lucas</title>
 </head>
 <body>
